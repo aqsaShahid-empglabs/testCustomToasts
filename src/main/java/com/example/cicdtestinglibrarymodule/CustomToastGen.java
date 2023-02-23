70d5c34bfbdb4c34a72d9a30481ddd3a4b07150e
aqsaShahid-empglabs <aqsa.shahid@empglabs.com>
2023-02-23 03:15:06 -0800

    New file

---
+++ src/main/java/com/example/cicdtestinglibrarymodule/CustomToastGen.java
@@ -0,0 +1,14 @@
+package com.example.cicdtestinglibrarymodule;
+
+import android.util.Log;
+
+public class CustomToastGen {
+    public static void generateToast(){
+        Log.e("TOAST GENERATED","123");
+        Log.e("TOAST GENERATED","123");
+        Log.e("TOAST GENERATED","123");
+        Log.e("TOAST GENERATED","123");
+        Log.e("TOAST GENERATED","123");
+        Log.e("TOAST GENERATED","123");
+    }
+}
