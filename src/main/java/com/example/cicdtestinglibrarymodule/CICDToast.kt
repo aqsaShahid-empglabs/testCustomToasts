b4a9c93a7d8517bdf535e698f974184df52e4840
aqsaShahid-empglabs <aqsa.shahid@empglabs.com>
2023-02-23 02:16:59 -0800

    Impl

--- src/main/java/com/example/cicdtestinglibrarymodule/CICDToast.kt
+++ src/main/java/com/example/cicdtestinglibrarymodule/CICDToast.kt
@@ -1,4 +1,12 @@
 package com.example.cicdtestinglibrarymodule
 
+import android.content.Context
+import android.widget.Toast
+
 class CICDToast {
+    companion object{
+         fun publistToast(context: Context, string: String){
+             Toast.makeText(context,string,Toast.LENGTH_LONG)
+         }
+    }
 }
